e3c088be5a2d25a9f8bd578296b074eb00f98b45
Alex Kosova <whovian2504@yandex.ru>
2023-09-22 00:09:21 +0300

    env

--- routes/routes.js
+++ routes/routes.js
@@ -24,8 +24,6 @@ routes.post('/signup', celebrate({
     email: Joi.string().required().email(),
     password: Joi.string().required().min(2),
     name: Joi.string().min(2).max(30),
-    about: Joi.string().min(2).max(30),
-    avatar: Joi.string().pattern(/^https?:\/\/(?:www\.)?[a-zA-Z0-9а-яА-Я-._~:/?#[\]@!$&'()*+,;=]+/im),
   }),
 }), createUser);
 
