9d8190022bdc1281399e84979525210adc7c0ed7
Alex Kosova <whovian2504@yandex.ru>
2023-09-21 23:37:20 +0300

    env

--- models/movie.js
+++ models/movie.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 
-const cardSchema = new mongoose.Schema(
+const movieSchema = new mongoose.Schema(
   {
     country: {
       type: String,
