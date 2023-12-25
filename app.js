eff9c5ede3d006744ad47db113d98e42fb79dae4
AlexKosova <alexkosowa@gmail.com>
2023-12-25 20:10:05 +0300

    правка адреса фронта

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const { requestLogger, errorLogger } = require('./middlewares/logger');
 const allowedCors = [
   'http://movies-explorer.alexk.nomoredomainsrocks.ru',
   'https://movies-explorer.alexk.nomoredomainsrocks.ru',
-  'http://127.0.0.1:3001',
+  'http://127.0.0.1:3000',
   'http://localhost:3000',
   'http://localhost:3001',
 ];
