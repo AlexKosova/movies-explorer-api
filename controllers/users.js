4c35f9f2c0de82c65c1f0284f8eb5007f097ced4
AlexKosova <116948508+AlexKosova@users.noreply.github.com>
2024-01-23 19:35:50 +0300

    Update users.js

--- controllers/users.js
+++ controllers/users.js
@@ -46,6 +46,7 @@ const login = (req, res, next) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_KEY : 'secret-11', {
         expiresIn: '7d',
       });
+      user.jwt = token;
       res.cookie('jwt', token);
       res.send(user.toJSON());
     })
